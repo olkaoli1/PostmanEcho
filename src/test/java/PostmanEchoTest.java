d603bb2b142d80c35c1026e32d4c6498caa585e7
Olga <oli.samsonowa@yandex.ru>
2025-01-25 13:46:07 +0300

    Добавление проекта PostmanEcho для выполнения задачи

---
+++ src/test/java/PostmanEchoTest.java
@@ -0,0 +1,21 @@
+import io.restassured.RestAssured;
+import org.junit.jupiter.api.Test;
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+
+public class PostmanEchoTest {
+
+    @Test
+    public void shouldReturnSentData() {
+        RestAssured.baseURI = "https://postman-echo.com";
+
+        given()
+                .contentType("text/plain; charset=UTF-8")
+                .body("some data")
+                .when()
+                .post("/post")
+                .then()
+                .statusCode(200)
+                .body("data", equalTo("some data"));
+    }
+}
